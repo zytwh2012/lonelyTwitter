474e6df93570e2b0240cf7f6b9ef1d26c8aa50b5
ssd352 <27953063+ssd352@users.noreply.github.com>
2017-09-14 19:05:33 -0600

    Add some classes

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweetable.java
@@ -0,0 +1,13 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.Date;
+
+/**
+ * Created by dezfuli on 9/14/17.
+ */
+
+public interface Tweetable {
+    public String getMessage();
+
+    public Date getDate();
+}
